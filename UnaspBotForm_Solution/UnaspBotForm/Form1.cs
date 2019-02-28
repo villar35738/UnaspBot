37e21c18a6b2c147b939db78f9f4155b1c206d47
Daniel Vilar <danielvillar1998@outlook.com>
2019-02-28 15:16:26 -0300

    Adicionando a Solution vinculada ao Google Calendar API

--- UnaspBotForm_Solution/UnaspBotForm/Form1.cs
+++ UnaspBotForm_Solution/UnaspBotForm/Form1.cs
@@ -13,11 +13,17 @@ using OpenQA.Selenium.Chrome;
 using System.Threading;
 using System.IO;
 using System.Web.Script.Serialization;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace UnaspBotForm
 {
     public partial class Form1 : Form
     {
+        static string[] Scopes = { CalendarService.Scope.Calendar };
         public Form1()
         {
             InitializeComponent();
@@ -129,12 +135,15 @@ namespace UnaspBotForm
                     {
                         listTask.Items.Add("Data de entrega: " + time);
                     });
+                    time = item.FindElement(By.TagName("time")).GetAttribute("datetime");
 
                     status = item.FindElement(By.ClassName("snap-completion-meta")).GetAttribute("innerText");
                     this.Invoke((MethodInvoker)delegate
                     {
                         listTask.Items.Add("Status: " + status + "\n");
                     });
+
+                    createEventCalendar(calendar(), titulo, materia, time, status);
 
                     this.Invoke((MethodInvoker)delegate
                     {
@@ -221,6 +230,91 @@ namespace UnaspBotForm
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public static CalendarService calendar()
+        {
+            // GOOGLE CALENDAR
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(AppDomain.CurrentDomain.BaseDirectory + "//credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var googleCalendar = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "UnaspBot",
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = googleCalendar.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // END GOOGLE CALENDAR
+
+            return googleCalendar;
+        }
+
+        public static void createEventCalendar(CalendarService calendar, string titulo, string materia, string dataFim, string status)
+        {
+            // Define parameters of request.
+            EventsResource.ListRequest request = calendar.Events.List("primary");
+            request.TimeMin = Convert.ToDateTime(dataFim).AddHours(-24);
+            request.TimeMax = Convert.ToDateTime(dataFim);
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            Events eventsList = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (eventsList.Items != null && eventsList.Items.Count > 0)
+            {
+                foreach (var eventItem in eventsList.Items)
+                {
+                    if (eventItem.Description.Equals(titulo + "\n" + status))
+                    {
+                        calendar.Events.Delete("primary", eventItem.Id).Execute();
+                        break;
+                    }
+                }
+            }
+            Event newEvent = new Event()
+            {
+                Summary = materia.Split('-')[0].Trim(),
+                Description = titulo + "\n" + status,
+                Start = new EventDateTime()
+                {
+                    DateTime = Convert.ToDateTime(dataFim).AddHours(-1),
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = Convert.ToDateTime(dataFim),
+                },
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest requestCreate = calendar.Events.Insert(newEvent, calendarId);
+            Event createdEvent = requestCreate.Execute();
+            Console.WriteLine("Event created: {0}", createdEvent.HtmlLink);
         }
     }
 }
--- UnaspBot_Solution/UnaspBot/Program.cs
+++ UnaspBot_Solution/UnaspBot/Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium.PhantomJS;
+﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support;
 using OpenQA.Selenium;
 using System;
@@ -8,17 +8,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace TesteRobo
 {
     class Program
     {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+       //static string ApplicationName = "Google Calendar API .NET Quickstart";
+
         static void Main(string[] args)
         {
-            var service = PhantomJSDriverService.CreateDefaultService();
-            service.AddArgument("--webdriver-loglevel=NONE");
+            var serviceDriver = new ChromeOptions();
+            serviceDriver.AddArgument("--headless");
 
-            PhantomJSDriver webTeste = new PhantomJSDriver(service);
+            ChromeDriver driver = new ChromeDriver(serviceDriver);
             string nomeLogin = "", senha = "";
             var materia = "";
             var titulo = "";
@@ -41,21 +49,21 @@ namespace TesteRobo
                 }
             }
             Console.WriteLine("\n--------- Redirecionando para https://unasp.mrooms.net/login/index.php ---------");
-            webTeste.Navigate().GoToUrl("https://unasp.mrooms.net/login/index.php");
+            driver.Navigate().GoToUrl("https://unasp.mrooms.net/login/index.php");
             Console.WriteLine("------------------------------- Realizando o login -----------------------------");
-            webTeste.FindElementById("username").SendKeys(nomeLogin);
-            webTeste.FindElementById("password").SendKeys(senha);
+            driver.FindElementById("username").SendKeys(nomeLogin);
+            driver.FindElementById("password").SendKeys(senha);
 
-            webTeste.FindElementById("loginbtn").Click();
+            driver.FindElementById("loginbtn").Click();
 
             Thread.Sleep(2000);
             Console.WriteLine("-------------------------- Login efetuado com sucesso --------------------------");
-            webTeste.FindElementById("snap-pm-trigger").Click();
+            driver.FindElementById("snap-pm-trigger").Click();
             Console.WriteLine("----------------------- Exibindo todas as tarefas pendentes --------------------");
 
             Thread.Sleep(1500);
 
-            var deadLines = webTeste.FindElementsByXPath("//div[@id='snap-personal-menu-deadlines']/div");
+            var deadLines = driver.FindElementsByXPath("//div[@id='snap-personal-menu-deadlines']/div");
 
             if (deadLines.Count == 0)
             {
@@ -66,13 +74,13 @@ namespace TesteRobo
                 Thread.Sleep(1000);
                 Console.Write("1");
                 Thread.Sleep(1000);
-                deadLines = webTeste.FindElementsByXPath("//div[@id='snap-personal-menu-deadlines']/div");
+                deadLines = driver.FindElementsByXPath("//div[@id='snap-personal-menu-deadlines']/div");
 
                 if (deadLines.Count == 0)
                 {
                     Console.WriteLine("\nO sistema não conseguiu se conectar com o servidor. Por favor, verifique sua conexão e tente novamente...");
                     Console.ReadKey();
-                    webTeste.Dispose();
+                    driver.Dispose();
                     Environment.Exit(0);
                 }
                 Console.WriteLine("");
@@ -85,6 +93,7 @@ namespace TesteRobo
             foreach (var item in deadLines)
             {
                 titulo = item.FindElement(By.TagName("h3")).GetAttribute("innerText");
+                
                 Console.WriteLine("Titulo: " + titulo.Split(delimiter).First());
 
                 materia = item.FindElement(By.TagName("small")).GetAttribute("innerText");
@@ -96,11 +105,79 @@ namespace TesteRobo
                 status = item.FindElement(By.ClassName("snap-completion-meta")).GetAttribute("innerText");
                 Console.WriteLine("Status: " + status + "\n");
 
+                createEventCalendar(calendar(), titulo + " - " + materia, time);
+
                 Console.WriteLine("********************************************************************************");
             }
 
             Console.ReadKey();
-            webTeste.Dispose();
+            driver.Dispose();
+        }
+
+        public static CalendarService calendar()
+        {
+            // GOOGLE CALENDAR
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(@"D:\GIT Projects\UnaspBot\UnaspBot_Solution\UnaspBot\credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var googleCalendar = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "UnaspBot",
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = googleCalendar.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // END GOOGLE CALENDAR
+
+            return googleCalendar;
+        }
+
+        public static void createEventCalendar(CalendarService calendar, string titulo, string dataFim)
+        {
+            Event newEvent = new Event()
+            {
+                Summary = "Google I/O 2015",
+                Location = "800 Howard St., San Francisco, CA 94103",
+                Description = "A chance to hear more about Google's developer products.",
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2019-20-02T09:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2015-05-28T17:30:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest request = calendar.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request.Execute();
+            Console.WriteLine("Event created: {0}", createdEvent.HtmlLink);
         }
     }
 }
